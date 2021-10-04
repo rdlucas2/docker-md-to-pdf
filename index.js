5573d8a927de8f3933cc3bea56311d820a794213
rdlucas2 <1649129+rdlucas2@users.noreply.github.com>
2021-10-04 13:31:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,5 @@
-const mdpdf = require('mdpdf');
+const log = require('loglevel');
+const app = require('./app');
 
-process.argv.forEach(function (val, index, array) {
-    console.log(index + ': ' + val);
-});
-
-let inputPath = process.argv[2];
-let outputPath = process.argv[3];
-let customCssPath = null;
-if(process.argv[4]) {
-    customCssPath = process.argv[4];
-}
-
-let options = {
-    source: inputPath, //'README.md'
-    destination: outputPath, //'output.pdf'
-    //styles: path.join(__dirname, 'md-styles.css'), //'md-styles.css' //moved to only add if provided in args
-    pdf: {
-        format: 'A4',
-        orientation: 'portrait'
-    }
-};
-
-if(customCssPath) {
-    options.styles = customCssPath
-}
-
-mdpdf.convert(options).then((pdfPath) => {
-    console.log('PDF Path:', pdfPath);
-    process.exit();
-}).catch((err) => {
-    console.error(err);
-    process.exit();
-})
+log.setLevel(log.levels.TRACE);
+app.run(process.argv, log);
